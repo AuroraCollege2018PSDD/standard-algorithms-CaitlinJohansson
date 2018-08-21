89156921de65855423d674f2515555bd4f66728a
Dat_Magpie <36179498+CaitlinJohansson@users.noreply.github.com>
2018-08-21 14:20:42 +1000

    Updated edition: things other than numbers
    If you input a letter it will now return to the main program as opposed to stopping and having a spaz

--- Lameo stuff for mini assessment.py
+++ Lameo stuff for mini assessment.py
@@ -1,7 +1,9 @@
 cont = True
 
 def loadArray(thisArray):
-        print("loadArray was called")
+        theArray.append(numbs)
+        print(theArray)
+        #append an array python
         
 def printArray(thisArray):
     outString = ""
@@ -9,7 +11,7 @@ def printArray(thisArray):
         outString = outString + str(num) + " "
     print(outString)
     numElements = len(thisArray)
-    print('there are {} elements in the array'.format(numElements))
+    print('There are {} elements in the array'.format(numElements))
 
 def sumArray(thisArray):
     sum = 0
@@ -25,14 +27,18 @@ def minArray(thisArray):
     
 
 #main program
-theArray = [1,2,3,4,5]
+theArray = []
 while cont:
     choice = input('Yo add something - L to add numbers to the array; P - print the dummy array; S - sum dat array; X - max of array; N - min of array; Q - quit this crappy program: ')
     if choice == 'l':
-        loadArray(theArray)
-                
+        try:
+            numbs = int(input('Enter a number of your choice to put into the array: '))
+        except ValueError:
+          print("That's not an int!")
+        else:
+            loadArray(theArray)
     elif choice == 'p':
-        print(theArray)
+        printArray(theArray)
     elif choice == 's':
         sumArray(theArray)
     elif choice == 'x':
